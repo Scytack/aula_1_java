6484e8ec4f2129e72586ae6a73f23e63dec6628c
Scytack <155610957+Scytack@users.noreply.github.com>
2025-04-09 14:53:11 -0300

    Add files via upload

---
+++ exer_11.js
@@ -0,0 +1,6 @@
+//Concatenação
+var nome = "Arthur"
+var idade ="18"
+var cidade = "Valença"
+
+console.log("Olá! Meu nome é " + nome + "\n Tenho " + idade + " anos." + "\n Moro em " + cidade)
---
+++ exer_6.js
@@ -0,0 +1,6 @@
+// A variavel recebe um nome e exibe o valor dele
+var nome = "Arthur"
+console.log(nome)
+// A variavel recebe uma idade e exibe o valor dela
+var idade = "18"
+console.log(idade)
