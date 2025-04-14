99ac68604640acba913f5d5a0cc938f0bea8b1b4
Arthur Paiva <155610957+Scytack@users.noreply.github.com>
2025-04-14 13:11:18 -0300

    Add files via upload

---
+++ www/aula_2/tipo.js
@@ -0,0 +1,56 @@
+var nome = "Maria";
+console.log(typeof nome) // String
+
+var idade = 30;
+console.log(typeof idade) // Number
+
+var estudando = true;
+console.log(typeof estudando) // Boolean
+
+var sobrenome = "Silva";
+console.log(typeof sobrenome) // String
+
+var saldo = 0;
+console.log(typeof saldo) // Number
+
+var indefinido;
+console.log(typeof indefinido) // undefined
+
+var cidade = "Rio de Janeiro"; 
+console.log(typeof cidade) // String
+
+var numeroGrande = 1000000;
+console.log(typeof numeroGrande) // Number
+
+var curso = "JavaScript";
+console.log(typeof curso) // String
+
+var temperatura = -5;
+console.log(typeof temperatura) // Number
+
+var saudacao = "Olá, tudo bem?";
+console.log(typeof saudacao) // String
+
+var notaProva = 8.75;
+console.log(typeof notaProva) // Number
+
+var nada = null;
+console.log(typeof nada) // Object
+
+var numeroComoTexto = "123";
+console.log(typeof numeroComoTexto) // String
+
+var lista = [10, 20, 30];
+console.log(typeof lista) // Object
+
+var objeto = {marca: "Nike", tamanho: 42};
+console.log(typeof objeto) // Object
+
+var anoNascimento = 1993;
+console.log(typeof anoNascimento) // Number
+
+var vazio = "";
+console.log(typeof vazio) // String
+
+var altura = 1.65;
+console.log(typeof altura) // Number
