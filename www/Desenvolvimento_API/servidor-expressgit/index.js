c00522744e122ee13590b1f914239b9f06328cf8
Arthur Paiva <155610957+Scytack@users.noreply.github.com>
2025-05-12 16:47:09 -0300

    Add files via upload

--- www/Desenvolvimento_API/servidor-expressgit/index.js
+++ www/Desenvolvimento_API/servidor-expressgit/index.js
@@ -1,20 +1,29 @@
- const express = require('express')
- const app = express()
+const express = require('express')
 
- app.use(express.json())
+const app = express()
 
- app.get('/', (req, res) => {
-    res.send("Bem vindo ao servidor express!!")
- })
+app.use(express.json())
 
- app.get('/sobre', (req, res) => {
-    res.send("Este é um projeto de exemplo com rotas.")
- })
+app.get('/', (req, res) => {
+    res.send('Servidor express funcionando!');
+})
 
- app.get('/contato', (req, res) => {
-    res.send("Entre em contato pelo e-mail:seuemail@email.com")
- })
+app.get('/usuario', (req , res) => {
+    res.send("Arthur")
+})
+
+app.get('/senai', (req , res) => {
+    const curso = "Back-End"
+    res.send(curso)
+})
+
+app.post('/usuario', (req , res) => {
+    const nome = req.body.nome
+    const cargo = req.body.cargo
+    const idade = req.body.idade
+    res.send("Usuario " + nome + " criado com sucesso, o seu cargo é " + cargo + " e sua idade é " + idade)
+})
 
 app.listen(3000, () => {
-    console.log("Seu servidor está funcionando em http://localhost:3000")
+    console.log('Servidor rodando em http://localhost:3000')
 })
