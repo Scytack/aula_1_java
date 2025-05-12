5aa0a215b6879f04753018e4f595cf323318fcf4
Arthur Paiva <155610957+Scytack@users.noreply.github.com>
2025-05-12 15:05:52 -0300

    Add files via upload

---
+++ www/Desenvolvimento_API/meu-servidorgit/index.js
@@ -0,0 +1,18 @@
+ const express = require('express')
+ const app = express()
+
+ app.get('/', (req, res) => {
+    res.send("Bem vindo ao servidor express!!")
+ })
+
+ app.get('/sobre', (req, res) => {
+    res.send("Este é um projeto de exemplo com rotas.")
+ })
+
+ app.get('/contato', (req, res) => {
+    res.send("Entre em contato pelo e-mail:seuemail@email.com")
+ })
+
+app.listen(3000, () => {
+    console.log("Seu servidor está funcionando em http://localhost:3000")
+})
