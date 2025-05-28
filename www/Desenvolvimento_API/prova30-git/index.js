9f22434c825c237d79509ff3aefd3bc217f482a1
Arthur Paiva <155610957+Scytack@users.noreply.github.com>
2025-05-28 13:38:08 -0300

    Add files via upload

--- www/Desenvolvimento_API/prova30-git/index.js
+++ www/Desenvolvimento_API/prova30-git/index.js
@@ -44,6 +44,36 @@ const conexao = mysql.createConnection({
   );
 })
 
+app.delete('/sessoes/:id', (req, res) => {
+  const {id} = req.params;
+  conexao.query('DELETE FROM sessoes WHERE id = ?', [id] , (err, results) => {
+
+    if(err) {
+      return res.status(500).send("Erro ao Deletar")
+  }
+  if (results.affectedRows === 0){
+      return res.status(404).send("Aluno não encontrado")
+  }
+     res.status(200).send("Aluno deletado com sucesso")
+})
+})
+
+app.put('/sessoes/:id', (req, res) =>{
+  const {id} = req.params;
+  const {aluno, personal, tipo_de_treino, data, horario, observacao} = req.body
+
+  const query = 'UPDATE sessoes SET aluno = ?, personal = ?, tipo_de_treino = ?, data = ?, horario = ?, observacao = ? WHERE id = ?';
+  conexao.query(query, [aluno, personal, tipo_de_treino, data, horario, observacao, id], (err, results) => {
+      if(err) {
+          return res.status(500).send("Erro ao atualizar");
+      }
+      if (results.affectedRows === 0){
+          return res.status(404).send("Aluno não encontrado")
+      }
+      res.status(200).send("Aluno atualizado com sucesso")
+  })
+})
+
   app.get('/sessoes', (req, res) => {
     conexao.query('SELECT aluno, personal, tipo_de_treino, data, horario, observacao FROM sessoes', (err, results) =>{
         if(err) {
@@ -52,6 +82,70 @@ const conexao = mysql.createConnection({
         res.status(200).send(results);
     })
   })
+
+  const planos = []
+
+  app.post('/planos', (req, res) => {
+    const {nome, duracao_meses, preco, descricao} = req.body;
+
+    if (!nome || typeof nome != 'string' || nome.trim() == ""){
+        return res.status(400).send('Nome do plano é obrigatório e não pode ser uma string vazia.');
+    }
+    if (!duracao_meses || typeof duracao_meses != 'number' || duracao_meses <= 0){
+        return res.status(400).send('A duração é obrigatória e não pode ser menor que 0.');
+    }
+    if (!preco || typeof preco != 'number' || preco <= 0){
+        return res.status(400).send('O tipo de treino é obrigatório e tem que ser um numero.');
+    }
+  
+  conexao.query(
+    'INSERT INTO planos (nome, duracao_meses, preco, descricao) VALUES (?, ?, ?, ?)',
+    [nome, duracao_meses, preco, descricao],
+    () => {
+        res.send("Plano cadastrado com sucesso!!");
+    }
+  );
+})
+
+app.delete('/planos/:id', (req, res) => {
+  const {id} = req.params;
+  conexao.query('DELETE FROM planos WHERE id = ?', [id] , (err, results) => {
+
+    if(err) {
+      return res.status(500).send("Erro ao Deletar")
+  }
+  if (results.affectedRows === 0){
+      return res.status(404).send("Plano não encontrado")
+  }
+     res.status(200).send("Plano deletado com sucesso")
+})
+})
+
+app.put('/planos/:id', (req, res) =>{
+  const {id} = req.params;
+  const {nome, duracao_meses, preco, descricao} = req.body
+
+  const query = 'UPDATE planos SET nome = ?, duracao_meses = ?, preco = ?, descricao = ? WHERE id = ?';
+  conexao.query(query, [nome, duracao_meses, preco, descricao, id], (err, results) => {
+      if(err) {
+          return res.status(500).send("Erro ao atualizar");
+      }
+      if (results.affectedRows === 0){
+          return res.status(404).send("Plano não encontrado")
+      }
+      res.status(200).send("Plano atualizado com sucesso")
+  })
+})
+
+  app.get('/planos', (req, res) => {
+    conexao.query('SELECT nome, duracao_meses, preco, descricao FROM planos', (err, results) =>{
+        if(err) {
+            return res.status(500).send('Erro ao buscar plano');
+        }
+        res.status(200).send(results);
+    })
+  })
+
 
   app.listen(3000, () => {
     console.log("Servidor backend rodando em http://localhost:3000")
